8fdf9af86757db3b36849ea6133bcadaf18375d8
Josef Raska <josef.raska@gmail.com>
2015-12-26 14:35:52 +0100

    Refactor ActivityBaseTest

--- travel-assistance/src/test/java/com/jraska/ActivityBaseTest.java
+++ travel-assistance/src/test/java/com/jraska/ActivityBaseTest.java
@@ -13,14 +13,14 @@ import java.lang.reflect.Method;
 public abstract class ActivityBaseTest<T extends Activity> extends BaseTest {
   //region Fields
 
-  private final Class<? extends Activity> _activityClass;
+  private final Class<T> _activityClass;
 
   //endregion
 
   //region Constructors
 
   public ActivityBaseTest() {
-    _activityClass = findActivityClass(getClass());
+    _activityClass = resolveActivityClass(getClass());
   }
 
   //endregion
@@ -44,33 +44,23 @@ public abstract class ActivityBaseTest<T extends Activity> extends BaseTest {
   //region Methods
 
 
-  private static Class<? extends Activity> findActivityClass(Class clazz) {
-    try {
-      return finActivtyClassUnchecked(clazz);
-    }
-    catch (NoSuchMethodException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
   @SuppressWarnings("unchecked")
-  private static Class<? extends Activity> finActivtyClassUnchecked(Class clazz)
-      throws NoSuchMethodException {
-    Class currentClazz = clazz;
+  private static <T extends Activity> Class<T> resolveActivityClass(Class clazz) {
     for (Class<?> c = clazz; c != Object.class; c = c.getSuperclass()) {
       for (Method method : c.getDeclaredMethods()) {
         if (method.getName().equals("getType")) {
-          return (Class<? extends Activity>) method.getReturnType();
+          return (Class<T>) method.getReturnType();
         }
       }
     }
-    throw new Error("Cannot determine correct type for getType() method.");
+
+    throw new Error("Cannot determine correct class for getType() method.");
   }
 
   /**
    * Method for reflection search for correct activity type through generics
    *
-   * @return Type fo tested activity
+   * @return Nothing
    */
   private T getType() {
     return null;
