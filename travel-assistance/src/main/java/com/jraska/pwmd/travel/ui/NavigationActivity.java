bb70b95e04cbac65e6a3db7b0e87dba70bb8ac00
Josef Raska <josef.raska@gmail.com>
2016-01-18 23:36:24 +0000

    Add arrow view

--- travel-assistance/src/main/java/com/jraska/pwmd/travel/ui/NavigationActivity.java
+++ travel-assistance/src/main/java/com/jraska/pwmd/travel/ui/NavigationActivity.java
@@ -3,11 +3,8 @@ package com.jraska.pwmd.travel.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import butterknife.Bind;
 import com.jraska.pwmd.travel.R;
 import com.jraska.pwmd.travel.TravelAssistanceApp;
 
@@ -17,7 +14,7 @@ public class NavigationActivity extends BaseActivity {
 
   //region Fields
 
-
+  @Bind(R.id.arrow_view) View _arrowView;
 
   //endregion
 
@@ -29,13 +26,21 @@ public class NavigationActivity extends BaseActivity {
     setContentView(R.layout.activity_navigation);
 
     TravelAssistanceApp.getComponent(this).inject(this);
+
+    // TODO: 18/01/16 Test code
+    updateDesiredDirection(45);
   }
 
   //endregion
 
   //region Methods
 
-  public static void startNavigationActivity(Activity fromActivity, UUID routeId){
+  protected void updateDesiredDirection(int degrees) {
+    // Rotation must be counter clockwise
+    _arrowView.setRotation(-degrees);
+  }
+
+  public static void startNavigationActivity(Activity fromActivity, UUID routeId) {
     Intent startNavigationIntent = new Intent(fromActivity, NavigationActivity.class);
     startNavigationIntent.putExtra(RouteDetailActivity.ROUTE_ID, routeId);
 
