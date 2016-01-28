2efd793f182df76c1aa0a506f44c05a8f770f6ed
Josef Raska <josef.raska@gmail.com>
2016-01-28 22:30:07 +0000

    Cosmetic if change

--- travel-assistance/src/main/java/com/jraska/pwmd/travel/ui/BaseActivity.java
+++ travel-assistance/src/main/java/com/jraska/pwmd/travel/ui/BaseActivity.java
@@ -39,10 +39,8 @@ public abstract class BaseActivity extends AppCompatActivity {
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == android.R.id.home) {
-      if (onNavigationIconClicked()) {
-        return true;
-      }
+    if (item.getItemId() == android.R.id.home && onNavigationIconClicked()) {
+      return true;
     }
 
     return super.onOptionsItemSelected(item);
