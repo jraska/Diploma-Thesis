e9e53b7ffe39d5f1359158228b44de7bd805c5fb
Josef Raska <josef.raska@gmail.com>
2016-02-14 13:35:56 +0000

    Add proper cleanup for file creating tests

--- travel-assistance/src/test/java/com/jraska/pwmd/travel/media/PicturesManagerTest.java
+++ travel-assistance/src/test/java/com/jraska/pwmd/travel/media/PicturesManagerTest.java
@@ -5,6 +5,8 @@ import com.jraska.BaseTest;
 import com.jraska.pwmd.travel.data.NoteSpec;
 import com.jraska.pwmd.travel.persistence.TravelDataRepository;
 import org.greenrobot.eventbus.EventBus;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -16,13 +18,36 @@ import static org.mockito.Mockito.when;
 
 public class PicturesManagerTest extends BaseTest {
 
+  //region Setup Methods
+
+  private File _picsDir;
+
+  @Before
+  public void preparePicsDir() throws Exception {
+    _picsDir = new File("./unit-test-pics");
+    if (!_picsDir.exists()) {
+      assertThat(_picsDir.mkdirs()).isTrue();
+    }
+  }
+
+  @After
+  public void cleanPicsDir() throws Exception {
+    for (File f : _picsDir.listFiles()) {
+      assertThat(f.delete()).isTrue();
+    }
+
+    assertThat(_picsDir.delete()).isTrue();
+  }
+
+  //endregion
+
   //region Test Methods
 
   @Test
   public void testGetIdForUri() throws Exception {
     UUID testId = UUID.randomUUID();
 
-    PicturesManager picturesManager = new PicturesManager(new File("."), new EventBus());
+    PicturesManager picturesManager = new PicturesManager(_picsDir, new EventBus());
     Uri pictureUri = picturesManager.createPictureUri(testId);
 
     UUID idForUri = picturesManager.getIdForUri(pictureUri);
@@ -35,7 +60,7 @@ public class PicturesManagerTest extends BaseTest {
     UUID testId = UUID.randomUUID();
     EventBus eventBus = new EventBus();
 
-    PicturesManager picturesManager = new PicturesManager(new File("."), eventBus);
+    PicturesManager picturesManager = new PicturesManager(_picsDir, eventBus);
     File imageFile = picturesManager.getImageFile(testId);
     if (imageFile.exists()) {
       assertThat(imageFile.delete()).isTrue();
--- travel-assistance/src/test/java/com/jraska/pwmd/travel/media/SoundsManagerTest.java
+++ travel-assistance/src/test/java/com/jraska/pwmd/travel/media/SoundsManagerTest.java
@@ -4,6 +4,8 @@ import com.jraska.BaseTest;
 import com.jraska.pwmd.travel.data.NoteSpec;
 import com.jraska.pwmd.travel.persistence.TravelDataRepository;
 import org.greenrobot.eventbus.EventBus;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,13 +16,32 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SoundsManagerTest extends BaseTest {
+  private File _soundsDir;
+
+  @Before
+  public void prepareSoundsDir() throws Exception {
+    _soundsDir = new File("./unit-test-sounds");
+    if (!_soundsDir.exists()) {
+      assertThat(_soundsDir.mkdirs()).isTrue();
+    }
+  }
+
+  @After
+  public void cleanSoundsDir() throws Exception {
+    for (File f : _soundsDir.listFiles()) {
+      assertThat(f.delete()).isTrue();
+    }
+
+    assertThat(_soundsDir.delete()).isTrue();
+  }
+
   @Test
   public void testSoundFileDeletedOnNoteSpecDeleted() throws Exception {
 
     UUID testId = UUID.randomUUID();
     EventBus eventBus = new EventBus();
 
-    SoundsManager soundsManager = new SoundsManager(new File("."), eventBus);
+    SoundsManager soundsManager = new SoundsManager(_soundsDir, eventBus);
     File soundFile = soundsManager.getFileForId(testId);
     if (soundFile.exists()) {
       assertThat(soundFile.delete()).isTrue();
